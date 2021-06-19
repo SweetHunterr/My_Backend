52a6bab5a35ef1532000ea98cf21288f1c15ca6d
SweetHunterr <85028631+SweetHunterr@users.noreply.github.com>
2021-06-19 21:24:46 +0700

    Tugas Web

--- index.js
+++ index.js
@@ -1,64 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
-const mysql = require("mysql");
-const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_deskripsi",
-});
+const routerTodo = require("./routers/todo.js");
+const routerUser = require("./routers/user.js");
 
-mysqlConnection.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Terkoneksi Ke Database");
-  }
-});
+app.use(routerTodo);
+app.use(routerUser);
 
-app.get("/", (req, res) => {
-  res.send(
-    '<html><body><form action="/todo" method="post"><input name="deskripsi"/><button>Add</button></form></body></html>'
-  );
-  res.end();
-});
-
-app.post("/todo", (req, res) => {
-  const data = req.body.deskripsi;
-  mysqlConnection.query(
-    "insert into tb_deskripsi values(null, '" + data + "')",
-    (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Data Telah Ditambahkan Ke Database. Terima Kasih :)");
-        res.end();
-      }
-    }
-  );
-});
-
-app.get("/todo", (req, res) => {
-  mysqlConnection.query("select * from tb_deskripsi", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-app.delete("/todo/:id", (req, res) => {
-  const { id } = req.params;
-  mysqlConnection.query(`delete from tb_deskripsi where id = ${id}`);
-});
-
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Server Sudah Berjalan ... ");
+});
+
+io.on("connection", (socket) => {
+  console.log("Pengguna terhubung : " + socket.id);
+
+  socket.on("getTodo", (data) => {
+    socket.broadcast.emit("getTodo", data);
+  });
 });
